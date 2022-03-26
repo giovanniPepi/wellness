c7a53c9685c0e0cc1bc315e5b9152a4d1e12936d
gtpepi <gtpepi@hotmail.com>
2022-03-26 13:16:01 -0300

    Add button handlers, feelsgood variable, contentupdater

--- js/contentUpdater.js
+++ js/contentUpdater.js
@@ -1,16 +1,35 @@
-contentCleaner = () => {
-    const introQuest = document.querySelector(".introQuestion");
+let feelsBetter = false;
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+contentUpdater = (arg1, arg2) => {
     const content = document.querySelector(".content");
-
-    introQuest.innerHTML = "";
-    content.innerHTML = "";
+    const introQuest = document.querySelector(".introQuestion");
+    content.textContent = arg1;
+    introQuest.textContent = arg2;
 }
 buttonUpdater = (arg1, arg2) => {
-    button1 = document.querySelector("#button1");
-    button2 = document.querySelector("#button2");
-    button1.textContent = arg1; // add
-    button2.textContent = arg2; //add
+    button1.textContent = arg1;
+    button2.textContent = arg2;
 }
+choiceHandler = () => {    
+    button1.addEventListener("click", () => {
+        feelsBetter = false;
+        alert("sorry!")
+    })
+    button2.addEventListener("click", () => {
+        feelsBetter = true;
+        feelsBetter();
+    });
+}
+feelsBetterHandler = () => {
+    if (!feelsBetter) return;
+    const text1 = "congratulations! You've solved your problem!";
+    const text2 = "";
+    contentUpdater(text1, text2);
+};
+choiceHandler();
+
+
 
 
 
