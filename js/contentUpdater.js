8d071bbc00ae47951caf5373685dc2c6d0d2360d
gtpepi <gtpepi@hotmail.com>
2022-03-26 14:13:37 -0300

    Update buttonlistenerupdater

--- js/contentUpdater.js
+++ js/contentUpdater.js
@@ -1,9 +1,11 @@
 let feelsBetter = false;
+let shouldAsk = false;
 let step = 0;
 
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const buttons = document.querySelector(".buttons"); 
+const sidebar = document.querySelector(".sidebar");
 
 contentUpdater = (arg1, arg2) => {
     const content = document.querySelector(".content");
@@ -16,8 +18,7 @@ buttonUpdater = (arg1, arg2) => {
     button2.textContent = arg2;
 }
 buttonChoiceCreator = (arg1, arg2) => {
-    buttons.removeChild(button1);
-    buttons.removeChild(button2);
+    buttons.innerHTML="";
     btn3 = document.createElement("button");
     btn3.setAttribute("class", "button");
     btn4 = document.createElement("button");
@@ -42,22 +43,37 @@ feelsBetterHandler = () => {
     const text1 = "congratulations! You've solved your problem!";
     const text2 = "come back whenever you need it =)";
     contentUpdater(text1, text2);
-    buttonUpdater("", "");
+    buttons.innerHTML="";
 };
+function sidebarUpdater (step){
+    sidebar.textContent = step;
+}
+function buttonListenerUpdater () {
+    btn3.addEventListener("click", () => {
+        
+    });
+    btn4.addEventListener("click", () => {
+        feelsBetter = true;
+        feelsBetterHandler();        
+    })
+
+}
 
 /* PLEASE INCLUDE SOURCES! */
 function happinessGenerator (step) {
     if (feelsBetter) return;
     switch (step) {
         case 0:
-            contentUpdater("0 - Lack of proper hydration can lead to mood swings, headaches and " +
+            sidebarUpdater(step);
+            contentUpdater("Lack of proper hydration can lead to mood swings, headaches and " +
             "can be tiring", "Did you drink water in the past few hours?")
             buttonChoiceCreator("No - I will drink now", "Yes, I'm hydrated");
+            buttonListenerUpdater();
+
             break;
         case 1:
         }
     }
-
 
 choiceHandler();
 
