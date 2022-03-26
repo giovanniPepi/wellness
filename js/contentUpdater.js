af7a2a7bfcdcbb560edff3978ce1cd0d12be3442
gtpepi <gtpepi@hotmail.com>
2022-03-26 14:05:00 -0300

    Add new buttons creator, update css style for their div

--- js/contentUpdater.js
+++ js/contentUpdater.js
@@ -3,6 +3,8 @@ let step = 0;
 
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
+const buttons = document.querySelector(".buttons"); 
+
 contentUpdater = (arg1, arg2) => {
     const content = document.querySelector(".content");
     const introQuest = document.querySelector(".introQuestion");
@@ -13,27 +15,28 @@ buttonUpdater = (arg1, arg2) => {
     button1.textContent = arg1;
     button2.textContent = arg2;
 }
+buttonChoiceCreator = (arg1, arg2) => {
+    buttons.removeChild(button1);
+    buttons.removeChild(button2);
+    btn3 = document.createElement("button");
+    btn3.setAttribute("class", "button");
+    btn4 = document.createElement("button");
+    btn4.setAttribute("class", "button");
+    btn3.textContent = arg1;
+    btn4.textContent = arg2;
+    buttons.appendChild(btn3);
+    buttons.appendChild(btn4);
+};
 choiceHandler = () => {    
     button1.addEventListener("click", () => {
         feelsBetter = false;
         happinessGenerator(step);
-    })
+    })  
     button2.addEventListener("click", () => {
         feelsBetter = true;
         feelsBetterHandler();
     });
-}/* 
-choiceStepHandler = () => {
-    button1.addEventListener("click", () => {
-        contentUpdater("if you just drank water, let a few minutes pass so your body can recover its proper hydration. ",
-        "Do you feel better now?");
-        buttonUpdater("I still feel bad.", "I feel fine!");       
-    })
-    button2.addEventListener("click", () => {
-        feelsBetter = true;
-        feelsBetterHandler();
-    });
-} */
+}
 feelsBetterHandler = () => {
     if (!feelsBetter) return;
     const text1 = "congratulations! You've solved your problem!";
@@ -47,22 +50,11 @@ function happinessGenerator (step) {
     if (feelsBetter) return;
     switch (step) {
         case 0:
-            contentUpdater("Lack of proper hydration can lead to mood swings, headaches and " +
-            "can be tiring", "Did you drink water in the past few hours?");
-            buttonUpdater("I will drink water now", "I'm hydrated");
-            button1.addEventListener("click", () => {
-                contentUpdater("if you just drank water, let a few minutes pass so your body can recover its proper hydration. ",
-                "Do you feel better now?");
-                buttonUpdater("I still feel bad.", "I feel fine!");  
-                button1.addEventListener("click", () => {
-                    step++;
-                    happinessGenerator(step);
-                })     
-            });
+            contentUpdater("0 - Lack of proper hydration can lead to mood swings, headaches and " +
+            "can be tiring", "Did you drink water in the past few hours?")
+            buttonChoiceCreator("No - I will drink now", "Yes, I'm hydrated");
             break;
-        case 1: {
-            
-        }
+        case 1:
         }
     }
 
