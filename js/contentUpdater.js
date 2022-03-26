7495b60eb6b24c1a9e41569a29e69d89bfab7a03
gtpepi <gtpepi@hotmail.com>
2022-03-26 12:57:09 -0300

    Add content cleaner

---
+++ js/contentUpdater.js
@@ -0,0 +1,11 @@
+contentCleaner = () => {
+    const introQuest = document.querySelector(".introQuestion");
+    const content = document.querySelector(".content");
+
+    introQuest.innerHTML = "";
+    content.innerHTML = "";
+}
+buttonUpdater = () => {
+
+}
+
